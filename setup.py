121bafda27b77b868a997333fb009f23e4f900c6
Alex Craig <acraig2@andrew.cmu.edu>
2023-04-26 19:32:17 +0000

    alsdj

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
+""" Here is my docstring so pylint is happy"""
+
 from setuptools import setup
 
-setup(name='ajcproject',
-      version='1.0.',
-      description='s23 final project',
-      maintainer='Alex Craig',
-      maintainer_email='acraig2@andrew.cmu.edu',
-      license='MIT',
-      packages=['ajcproject'],
-      entry_points={'console_scripts': ['oa = ajcproject.main:main']},
-      long_description='''This is my final project for the s23 mini 2''')
+setup(
+    name="ajcpkg",
+    version="1.0.",
+    description="s23 final project",
+    maintainer="Alex Craig",
+    maintainer_email="acraig2@andrew.cmu.edu",
+    license="MIT",
+    packages=["ajcpkg"],
+    entry_points={"console_scripts": ["oa = ajcpkg.main:main"]},
+    long_description="""This is my final project for the s23 mini 2""",
+)
